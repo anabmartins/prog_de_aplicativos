9961d596846ab72d89e229b213e0c01042739cad
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2023-03-01 08:40:57 -0300

    novo exemplo

--- 01-03/object.js
+++ 01-03/object.js
@@ -13,15 +13,16 @@ copia.teste = 2;
 
 console.log(obj.teste);
 
-// .exemplo 2
+// exemplo 2
 // cria um objeto "pessoa"
-const pessoa = {
+let pessoa = {
   nome: 'Maria',
-  idade: 30
+  idade: 30,
+  altura: 1.70
 };
 
 // atribui a referência do objeto "pessoa" para outra variável chamada "outraPessoa"
-const outraPessoa = pessoa;
+let outraPessoa = pessoa;
 
 // modifica o valor da propriedade "idade" do objeto "outraPessoa"
 outraPessoa.idade = 35;
@@ -29,3 +30,17 @@ outraPessoa.idade = 35;
 // imprime os valores das propriedades "idade" da "pessoa" e da "outraPessoa" (ambas devem ser 35)
 console.log(pessoa.idade);
 console.log(outraPessoa.idade);
+
+// exemplo 3
+
+let aula = {
+  horas: 2,
+  materia: 'programacao de apps',
+  profesor: 'Bruno',
+  quarta: true
+}
+
+testes = aula.materia = 'testes'
+quarta = aula.quarta = false
+
+console.log(aula);
