be8c3e5a56cef10f4d4270b0b3f12ad08b95f2c7
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2023-03-08 12:43:09 -0300

    Add files via upload

--- 08-03/lista.js
+++ 08-03/lista.js
@@ -1,3 +1,4 @@
+// 1 e 2 
 function Ninja(name, shurikens) {
     this.name = name;
     this.shurikens = shurikens;
@@ -14,10 +15,34 @@ function Ninja(name, shurikens) {
     }
   }
   
-  const meuNinja = new Ninja('Naruto');
+  // const meuNinja = new Ninja('Naruto');
   console.log(meuNinja.name); 
   meuNinja.atirarShuriken(5); 
 
-  
+// 3
+function Inimigo(nome){
+  this.nome = nome;
+  this.vivo = true;
+  this.atirarShuriken = function() {
+    if (this.vivo && this.shuriken > 0) {
+      console.log(`${this.nome} foi atingido por um shuriken e morreu!`);
+      this.vivo = false;
+      this.shuriken = 0;
+    } else if (this.vivo) {
+      console.log(`${this.nome} desviou do shuriken!`);
+    } else {
+      console.log(`${this.nome} já está morto!`);
+    }
+  }
+}
 
+const inimigo = new Inimigo('Sasuske')
+console.log(inimigo.nome);
+console.log(inimigo.vivo);
+inimigo.atirarShuriken()
+
+const meuNinja = new Ninja('Naruto', 1);
+meuNinja.atirarShuriken(inimigo); // 'Gato Negro foi atingido por um shuriken e morreu!'
+console.log(inimigo.vivo); // false
+inimigo.atirarShuriken(); // 'Gato Negro já está morto!'
 
